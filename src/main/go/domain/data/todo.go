f0838e3706a85eef4fe086545c2ce1f4398609f8
Md Najim Ahmed <najim.ju@gmail.com>
2023-01-19 15:09:24 +0800

    update structure

--- src/main/go/domain/data/todo.go
+++ src/main/go/domain/data/todo.go
@@ -3,7 +3,6 @@ package data
 import (
 	"github.com/lib/pq"
 	"gorm.io/gorm"
-	"time"
 )
 
 type Todo struct {
@@ -23,13 +22,13 @@ type Todo struct {
 //}
 
 type OrMapping struct {
-	ID           uint      `gorm:"primarykey"`
-	OrId         int       `json:"or_id"`
-	SubSpecialty string    `json:"sub_specialty"`
-	Weekday      string    `json:"weekday"`
-	OpeningTime  time.Time `json:"opening_time"`
-	ClosingTime  time.Time `json:"closing_time"`
-	AnesthType   string    `json:"anesth_type"`
+	ID           uint   `gorm:"primarykey"`
+	OrId         int    `json:"or_id"`
+	SubSpecialty string `json:"sub_specialty"`
+	Weekday      string `json:"weekday"`
+	OpeningTime  string `json:"opening_time"`
+	ClosingTime  string `json:"closing_time"`
+	AnesthType   string `json:"anesth_type"`
 	//WeekId       []int  `json:"week_id"`
 	WeekId pq.Int32Array `json:"week_id" gorm:"type:int[]"`
 }
